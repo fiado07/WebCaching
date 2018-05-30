0bfc5a83476ce22ea97e138276be078c0e3e5529
x120801 <fbaloi@millenniumbim.co.mz>
2018-05-30 15:50:55 +0200

    WebCaching First commint

---
+++ WebCaching/CachingManager/Cache.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web;
+using WebCaching.Contracts;
+
+namespace WebCaching.CachingManager
+{
+    public class Cache : ICache
+    {
+
+        private static readonly Lazy<Cache> lazyCache = new Lazy<Cache>(() => new Cache());
+
+        public static Cache Caching { get { return lazyCache.Value; } }
+
+
+        private Cache()
+        {
+                
+        }
+
+        public void AddCache(string key, object data)
+        {
+
+            HttpContext.Current.Cache.Insert(key, data);
+
+        }
+
+        public T GetCachedData<T>(string key)
+        {
+
+          T data = (T)HttpContext.Current.Cache.Get(key) ;
+
+            if (data == null)
+                data = default(T);
+
+            return data;  
+
+        }
+
+        public void RemoveCache(string key)
+        {
+            HttpContext.Current.Cache.Remove(key);
+        }
+    }
+}
---
+++ WebCaching/Contracts/ICache.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebCaching.Contracts
+{
+   public  interface ICache
+    {
+
+        void RemoveCache(string key);
+
+        void AddCache(string key, object data);
+
+        T GetCachedData<T>(string key);
+
+    }
+}
